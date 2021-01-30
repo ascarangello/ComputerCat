a6e4a1275a9df7fb305f7dc8ffcac49b98f8929f
ascarangello <33673583+ascarangello@users.noreply.github.com>
2021-01-30 17:43:16 -0500

    Designed first level, added sprite and walk anim

--- GGJ21/ComputerCat/Assets/Scripts/PlayerInputHandler.cs
+++ GGJ21/ComputerCat/Assets/Scripts/PlayerInputHandler.cs
@@ -8,14 +8,15 @@ public class PlayerInputHandler : MonoBehaviour
     public CharController controller;
     // Set ground movement speed
     private float movementSpeed = 65f;
-    
+    private Animator anim;
+    private bool walking = false;
     private float horizontalInput = 0f;
     private bool jumping = false;
     private bool dropMem = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,7 +25,7 @@ public class PlayerInputHandler : MonoBehaviour
         // Check for horizontal input
         horizontalInput = Input.GetAxisRaw("Horizontal") * movementSpeed;
         // Check if jump has been pressed this frame, jump at the end of the frame if so
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             jumping = true;
         } 
@@ -38,6 +39,16 @@ public class PlayerInputHandler : MonoBehaviour
     private void FixedUpdate()
     {
         // Tell our controller to move with the specified inputs
+        if (horizontalInput != 0.0 && walking == false)
+        {
+            walking = true;
+            anim.SetBool("Walking", true);
+        }
+        if (horizontalInput == 0.0 && walking == true)
+        {
+            walking = false;
+            anim.SetBool("Walking", false);
+        }
         controller.Move(horizontalInput * Time.fixedDeltaTime, jumping);
         jumping = false;
         if(dropMem)
