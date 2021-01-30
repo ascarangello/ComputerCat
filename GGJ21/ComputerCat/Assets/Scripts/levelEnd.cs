ee29799dabd4add0c7863c997c61145e832a5d6a
ascarangello <33673583+ascarangello@users.noreply.github.com>
2021-01-30 14:08:19 -0500

    screenwipe and level loading process, if you dont want to see the angry man disable screenwipe while working

---
+++ GGJ21/ComputerCat/Assets/Scripts/levelEnd.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+// Followed another brackeys tutorial for this:
+// https://www.youtube.com/watch?v=CE9VOZivb3I
+public class levelEnd : MonoBehaviour
+{
+    public Animator screenwipe;
+    public float transitionTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        screenwipe.SetTrigger("LevelEnd");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(index);
+
+    }
+}
