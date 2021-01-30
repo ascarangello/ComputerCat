94a219decfc93245107e2811672f5a7d1e41c5b5
ascarangello <33673583+ascarangello@users.noreply.github.com>
2021-01-30 11:32:24 -0500

    Second tile set version, fixed wall hanging, added cooler font, designed test level

--- GGJ21/ComputerCat/Assets/Scripts/CharController.cs
+++ GGJ21/ComputerCat/Assets/Scripts/CharController.cs
@@ -7,7 +7,7 @@ using UnityEngine.Events;
 // Removed parts about crouching
 public class CharController : MonoBehaviour
 {
-	private float m_JumpForce = 550f;                          // Amount of force added when the player jumps.
+	private float m_JumpForce = 600f;                          // Amount of force added when the player jumps.
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  // How much to smooth out the movement
 	[SerializeField] private bool m_AirControl = false;                         // Whether or not a player can steer while jumping;
 	[SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
--- GGJ21/ComputerCat/Assets/Scripts/PlayerInputHandler.cs
+++ GGJ21/ComputerCat/Assets/Scripts/PlayerInputHandler.cs
@@ -7,7 +7,7 @@ public class PlayerInputHandler : MonoBehaviour
     // CharController reference 
     public CharController controller;
     // Set ground movement speed
-    private float movementSpeed = 55f;
+    private float movementSpeed = 65f;
     
     private float horizontalInput = 0f;
     private bool jumping = false;
