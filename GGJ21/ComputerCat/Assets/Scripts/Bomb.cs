e65b738933365c41ba2bf53acc93d29601c7aac0
liamcristello <liamcristello@gmail.com>
2021-01-30 23:12:33 -0500

    AIM Guy walk speed
    Slowed down walk animation to line up with speed, attempting to fix bomb spawn issue.

--- GGJ21/ComputerCat/Assets/Scripts/Bomb.cs
+++ GGJ21/ComputerCat/Assets/Scripts/Bomb.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    private void Start()
+    {
+        Debug.Log("Bomb spawned!");
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Bomb exploded!");
         Destroy(gameObject);
     }
 }
