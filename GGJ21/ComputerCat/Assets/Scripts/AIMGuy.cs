b8429bf68aefa19fe06a4d2c27622f6e662acddb
liamcristello <liamcristello@gmail.com>
2021-01-30 23:38:43 -0500

    AIM Guy script
    Doesn't turn around correctly, but close

---
+++ GGJ21/ComputerCat/Assets/Scripts/AIMGuy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMGuy : Enemy
+{
+    protected override void UpdatePursuitState()
+    {
+        if (!playerDetected)
+        {
+            SwitchEnemyState(State.PATROL);
+        }
+        else
+        {
+            PatrolMovement();
+            Vector2 toTarget = (player.transform.position - transform.position).normalized;
+            if (Vector2.Dot(toTarget, transform.forward) < 0)
+            {
+                Debug.Log("Player got behind " + name + ", flip!");
+                Flip();
+            }
+        }
+    }
+}
